d36152cee1ccba938933611bc681178fd2d527b1
Daniel Dyla <dyladan@users.noreply.github.com>
2021-01-22 16:32:09 -0500

    add rubocop-rake

--- fluent-plugin-dynatrace.gemspec
+++ fluent-plugin-dynatrace.gemspec
@@ -20,4 +20,5 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'bundler'
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'rubocop'
+  gem.add_development_dependency 'rubocop-rake'
 end
--- lib/fluent/plugin/out_dynatrace.rb
+++ lib/fluent/plugin/out_dynatrace.rb
@@ -41,7 +41,7 @@ module Fluent
       config_section :buffer do
         config_set_default :chunk_keys, ['tag']
         config_set_default :flush_at_shutdown, true
-        config_set_default :chunk_limit_size, 200
+        config_set_default :chunk_limit_size, 10 * 1024
       end
 
       config_section :inject do
