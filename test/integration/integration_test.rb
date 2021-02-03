f11778a5231096c0b05d58f1bc370426efa6a50b
Daniel Dyla <dyladan@users.noreply.github.com>
2021-02-03 15:36:07 -0500

    move integration fixtures into test directory (#3)

--- test/integration/integration_test.rb
+++ test/integration/integration_test.rb
@@ -5,14 +5,13 @@ require 'net/http'
 
 class TestFluentIntegration < Test::Unit::TestCase
   def setup
-    puts `cd integration && docker-compose up -d --force-recreate --build`
-    # puts `cd integration && docker-compose down && docker-compose build && docker-compose up -d --force-recreate`
+    puts `cd test/integration/fixtures && docker-compose up -d --force-recreate --build`
     puts 'waiting 5s for integration test to start'
     sleep 5
   end
 
   def teardown
-    puts `cd integration && docker-compose down`
+    puts `cd test/integration/fixtures && docker-compose down`
   end
 
   def test_integration
@@ -28,10 +27,10 @@ class TestFluentIntegration < Test::Unit::TestCase
     puts 'waiting 10s for output plugin to flush'
     sleep 10
 
-    logs = `docker logs integration_logsink_1`
+    logs = `docker logs fixtures_logsink_1`
 
     line1 = '[{"foo":"bar"},{"abc":"def"},{"xyz":"123"},{"abc":"def"},{"xyz":"123"}]'
     line2 = '[{"abc":"def"},{"xyz":"123"}]'
-    assert_equal(logs, "#{line1}\n#{line2}\n")
+    assert_equal("#{line1}\n#{line2}\n", logs)
   end
 end
